7dc1ef5e98b8602709f6aa9cb725c5dca5f72813
alexiansan <alexian.sam.rousseau@gmail.com>
2020-01-19 18:52:11 +0100

    if else en 1 ligne

--- pages/views.py
+++ pages/views.py
@@ -10,8 +10,5 @@ from django.http import HttpResponse
 def homePageView(request):
     response = requests.get('http://api.syfaro.net/minecraft/1.3/server/status?ip=78.241.252.226&port=25565')
     server = response.json()
-    if server['status'] == 'success':
-        server['status'] = 'En ligne'
-    else:
-        server['status'] = 'Fermé'
+    server['status'] = 'En ligne' if server['status'] == 'success' else 'Fermé'
     return render(request, 'index.html', {'server' : server})

5ceea8b1bf7c5adcb9db19e481d2b7bbf9f0700e
alexiansan <alexian.sam.rousseau@gmail.com>
2020-01-19 19:23:52 +0100

    ajout images et favicon

--- pages/views.py
+++ pages/views.py
@@ -10,5 +10,5 @@ from django.http import HttpResponse
 def homePageView(request):
     response = requests.get('http://api.syfaro.net/minecraft/1.3/server/status?ip=78.241.252.226&port=25565')
     server = response.json()
-    server['status'] = 'En ligne' if server['status'] == 'success' else 'Fermé'
+    server['status'] = 'En ligne' if server['online'] and server['status'] == 'success' else 'Fermé'
     return render(request, 'index.html', {'server' : server})

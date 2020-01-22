5defef9ece333102055b9c64b2ed2dd33d718fb1
alexiansan <alexian.sam.rousseau@gmail.com>
2020-01-22 20:12:36 +0100

    amélioration du site v0.5

--- pages/views.py
+++ pages/views.py
@@ -10,5 +10,10 @@ from django.http import HttpResponse
 def homePageView(request):
     response = requests.get('http://api.syfaro.net/minecraft/1.3/server/status?ip=78.241.252.226&port=25565')
     server = response.json()
-    server['status'] = 'En ligne' if server['online'] and server['status'] == 'success' else 'Fermé'
+    if server['online'] and server['status'] == 'success':
+        server['status'] = 'En ligne'
+        server['color'] = 'green'
+    else:
+        server['status'] = 'Maintenance'
+        server['color'] = 'red'
     return render(request, 'index.html', {'server' : server})

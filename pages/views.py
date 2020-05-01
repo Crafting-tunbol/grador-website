b0055cea3e9162e299c203b68ff3577f9c2deaa1
alexiansan <alexian.sam.rousseau@gmail.com>
2020-05-01 17:08:57 +0200

    for beta test

--- pages/views.py
+++ pages/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 
 
 def homePageView(request):
-    response = requests.get('http://api.syfaro.net/minecraft/1.3/server/status?ip=78.241.252.226&port=25565')
+    response = requests.get('http://api.syfaro.net/minecraft/1.3/server/status?ip=51.68.244.62&port=25627')
     server = response.json()
     if server['online'] and server['status'] == 'success':
         server['status'] = 'En ligne'
